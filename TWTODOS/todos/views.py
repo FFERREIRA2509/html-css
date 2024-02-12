b7e8305ed219eea7be40c7ed4b8880555cded128
Fabiano Ferreira <fabianoferreira987@hotmail.com>
2024-02-12 13:43:58 -0300

    12.02.241

--- TWTODOS/todos/views.py
+++ TWTODOS/todos/views.py
@@ -1,3 +1,12 @@
 from django.shortcuts import render
+from django.http import HttpResponse
 
-# Create your views here.
+
+def artigos(request):
+    return render(request, "todos/artigos.html")
+
+
+#def home(request):
+ #   return render(request, "todos/home.html") 
+
+
